92f7773f4393ec77472b5d232f704343c8ff90bb
noemieleitao <leitao.noemie@gmail.com>
2018-02-02 17:50:51 +0100

    set structure

--- pages/header/controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//    define( "BASE_URL", "/catheview/");
-//    define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/catheview/");
-//
-//    echo ROOT_PATH;
-//    $bootstrapCssPath = ROOT_PATH."css/bootstrap/bootstrap.css";
-
-    include("header.php");
